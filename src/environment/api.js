238a51a5368644170d0937c36651954554fb79ae
biztech_V099 <biztech.trodex099@gmail.com>
2024-05-14 12:44:41 +0530

    changed url

--- src/environment/api.js
+++ src/environment/api.js
@@ -1,4 +1,4 @@
 
-const API_BASE_URL = "http://192.168.1.44:4000";
+const API_BASE_URL = "https://wingobackend.onrender.com";
 const API_LOGIN = "http://192.168.1.39:29388/WebService.asmx/ValidateLogin"
 export default API_BASE_URL;
