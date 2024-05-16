3741fc9d0eae6411f40db3efb4a4535b59a398af
abdulbiztech <abdulbiztech@gmail.com>
2024-05-16 12:02:18 +0530

    testing main netaws

--- src/environment/api.js
+++ src/environment/api.js
@@ -1,6 +1,8 @@
 //
 // const API_BASE_URL = "http://192.168.1.13:4000";
-const API_BASE_URL = "https://wingobackend.onrender.com";
+// const API_BASE_URL = "https://wingobackend.onrender.com";
+const API_BASE_URL = "https://wingobackend.decentrawood.com";
+
 
 const API_LOGIN = "http://192.168.1.39:29388/WebService.asmx/ValidateLogin"
 export default API_BASE_URL;
