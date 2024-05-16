1c4cf369805291129f2b7d3c976c7c5053046709
abdulbiztech <abdulbiztech@gmail.com>
2024-05-16 11:52:03 +0530

    testing main net

--- src/environment/api.js
+++ src/environment/api.js
@@ -1,6 +1,6 @@
 //
-const API_BASE_URL = "http://192.168.1.13:4000";
-// const API_BASE_URL = "https://wingobackend.onrender.com";
+// const API_BASE_URL = "http://192.168.1.13:4000";
+const API_BASE_URL = "https://wingobackend.onrender.com";
 
 const API_LOGIN = "http://192.168.1.39:29388/WebService.asmx/ValidateLogin"
 export default API_BASE_URL;
